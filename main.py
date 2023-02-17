bbdfe0377c29f6c3763a7ae9bafb2f305bdba2e3
Jason Le <jasonle2020@gmail.com>
2023-02-17 00:16:44 -0800

    add ranges for partial indexing and file writing

--- main.py
+++ main.py
@@ -19,21 +19,55 @@ def indexing(stem : str, filename : str) -> dict:
 def getFileName(path):
     return Path(path).stem
 
+def mergeDict(d1, d2):
+    #merging two dicts
+    merged_dict = defaultdict(list)
+    for d in (d1, d2):
+        for k, v in d.items():
+            merged_dict[k].append(v)
+
+    #sorting values which is a list object
+    for list in merged_dict.values():
+        for docID in list:
+            docID.sort(key=int)
+
+    return merged_dict
+
+#sepearting dictionary into term ranges
+def seperateDict(dict):
+    a_f = {}
+    g_l = {}
+    m_s = {}
+    t_z = {}
+
+    for k, v in dict.items():
+        if k[0] >= 'a' and k[0] <= 'f':
+            a_f[k] = v
+        elif k[0] >= 'g' and k[0] <= 'l':
+            g_l[k] = v
+        elif k[0] >= 'm' and k[0] <= 's':
+            m_s[k] = v
+        else:
+            t_z[k] = v
+
+    return [a_f, g_l, m_s, t_z]
 
 # import krovetz
 def main():
     
+
     #beginning path
     path  = 'DEV/'
 
-    #getting all files to be read
-
+    #getting all file names in order to be read n
     files = [f for f in glob(path + "**/*.json", recursive=True)]
     
     #loop thru every file
     # for each file tokenize, stem token, the create rev index for each file
     ps = PorterStemmer()
-    
+
+    all_file_names = ['a_f.json', 'g_l.json', 'm_s.json', 't_z.json']
+
     for file in files:
         with open(file, 'r') as f:
             data = json.load(f)
@@ -43,21 +77,56 @@ def main():
             # FIXME: 
             text = soup.get_text()
 
-            # punct_set =set(['!', '"', '#', '$' , '%', '&', "'", '(', ')','*', '+', ',', '-', '.', '/' , 
-            #                 ':' , ';' , '<', '=', '>' '?','@','[','\\',']','^','_','`','{','|','}','~', '-'])
-
             tokenized = word_tokenize(text)
-            tokenized = [token for token in tokenized if token not in string.punctuation]
-            stemmed = [ps.stem(s) for s in tokenized]
+            #make sure tokens are lowercase
+            tokenized = [token.lower() for token in tokenized if token not in string.punctuation]
+            #allows stemming to work with lowercase words
+            stemmed = [ps.stem(s, to_lowercase=True) for s in tokenized]
 
+            #getting partial_index from current html
             partial_inverted_index = indexing(stemmed, file)
-            json_data = json.dumps(partial_inverted_index, indent=2)
-            print(json_data)
+            #json_data = json.dumps(partial_inverted_index, indent=2)
+
+            #splitting partial index into 4 term ranges
+            sep_dicts = seperateDict(partial_inverted_index)
+
+            #write to those files and update dictionary from file and current index
             
-            #write into files
-            #with open(f'', 'w') as 
-            #sorting with alphabet 4 files ( 2 files with 6 letters and 2 files with 7 letters)
+            for i, current_dict in enumerate(sep_dicts):
+                
+                #file name to write to
+                with open(all_file_names[i], "r+") as f:
+                    #getting dict from file
+                    old_index = json.loads(f)
+
+                    #merging dict
+                    merged_dict = mergeDict(old_index, current_dict)
+
+                    #writing dict back to file
+                    f.seek(0)
+                    json.dump(merged_dict, f, indent=4)
+
+            
+    #TODO 
     
+    num_of_docs = 0
+    num_of_tokens = 0
+    file_size = 0
+    
+    #MERGE INDEX INTO ONE FILE
+    with open('report.txt', 'w') as file:
+
+        for name in all_file_names:
+            
+            with open(name, "r+") as f:
+                d = json.loads(f)
+                num_of_tokens += len(d.keys())
+                file_size += os.stat(name.st_size) / 1000
+                
+
+    #Generate stats report for deliverable
+
+            
         
 if __name__ == "__main__":
     main()
